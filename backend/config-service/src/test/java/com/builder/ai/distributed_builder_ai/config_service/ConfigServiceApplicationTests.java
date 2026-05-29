f05eb983c569c4ac441483a44236592e85a2d298
Siddhant Saxena <79580112+siddhant1599@users.noreply.github.com>
2026-05-29 19:03:57 -0400

    Added Config Files

---
+++ backend/config-service/src/test/java/com/builder/ai/distributed_builder_ai/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.builder.ai.distributed_builder_ai.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
